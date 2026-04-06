748c266e650c6e5b6aec4950c05b01e389de11c3
unnamedfurry <bememun80@gmail.com>
2026-04-06 22:17:35 +0300

    (bonus) added shaders state 1/3

---
+++ gaussian-blur.fs
@@ -0,0 +1,23 @@
+#version 330
+in vec2 fragTexCoord;
+out vec4 finalColor;
+
+uniform sampler2D texture0;
+uniform vec2 resolution; // texture size
+uniform vec2 direction; // vec2(1.0, 0.0) - horizontal, vec(0.0, 1.0) - vertical
+uniform float blurRadius;
+
+void main(){
+    vec4 color = vec4(0.0);
+    vec2 tex_offset = direction / resolution; // step in pixels
+    float weight[5] = float[](0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+    //float weight[5] = float[](0.0, 0.0, 0.0, 0.0, 0.0);
+
+    color += texture(texture0, fragTexCoord) * weight[0];
+
+    for (int i=0; i<5; i++){
+        color += texture(texture0, fragTexCoord + tex_offset * float(i)) * weight[i];
+        color += texture(texture0, fragTexCoord - tex_offset * float(i)) * weight[i];
+    }
+    finalColor = color;
+}
